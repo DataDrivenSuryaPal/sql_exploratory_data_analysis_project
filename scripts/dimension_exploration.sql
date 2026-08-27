407762719a78a3b91b38ded983d891b9cf7530d3
Surya pal <palsurya.947@gmail.com>
2026-08-27 21:26:47 +0530

    Add dimension exploration SQL script
    This script explores the structure of dimension tables, retrieving distinct countries from customers and product categories from the products table.

---
+++ scripts/dimension_exploration.sql
@@ -0,0 +1,21 @@
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+--Explore all countries our customers come from
+SELECT DISTINCT country FROM gold.dim_customers;
+--Explore all products categories 'The mejor division'
+SELECT DISTINCT 
+	category,
+	subcategory,
+	product_name 
+FROM gold.dim_products
+ORDER BY 1,2,3;
